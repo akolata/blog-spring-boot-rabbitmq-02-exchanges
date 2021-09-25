5006f387561e79855fcbec7261caa38b027909ec
Aleksander Kolata <olek.tbg.konta@gmail.com>
2021-09-25 14:24:01 +0200

    init project using Spring initializer

---
+++ src/test/java/pl/akolata/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akolata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
